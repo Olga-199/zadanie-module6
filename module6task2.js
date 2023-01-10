0d56e96aaa263009c1dade3193a20262024071b6
Olga-199 <121151860+Olga-199@users.noreply.github.com>
2023-01-10 05:45:13 +0300

    Add files via upload

---
+++ module6task2.js
@@ -0,0 +1,18 @@
+
+function getSimplicityOfNumber (number) {
+    if (number > 1000) return "Данные неверны.";
+    if (number === 0) return  "Ноль - есть ноль. Число ненатуральное.";
+    if (number === 1) return "Один - есть один. Не простой и не составной.";
+
+    let divisionsCount = 0;
+    for (let i = 1; i <= number; i++) {
+        if (number % i === 0) divisionsCount++;
+    }
+
+    if (divisionsCount === 2)
+        return "Число простое.";
+    else
+        return "Число составное.";
+}
+
+console.log(getSimplicityOfNumber(10));
